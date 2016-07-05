2904b5d0307b3d33b938ce090f2fa7ad16c19b2f
Rock <rocksevencolor@gmail.com>
2016-07-05 10:44:28 +0000

    Add kernal class

--- src/inter/Kernal.java
+++ src/inter/Kernal.java
@@ -1,17 +1,57 @@
 package inter;
 
+import java.util.Properties;
+
+import jdk.internal.dynalink.beans.StaticClass;
+import tool.ConfLoader;
+import tool.ConfLoader.ConfType;
+
 public class Kernal {
+	
+	public final static int
+		RANDOM=0,FIFO=1,LRU=2;
+	
 	private static Kernal kernal;
 	private Kernal(){}
+	
+	private static int replacePolicy=RANDOM;
+	public static int LABN=-1;
+	public static int PABN=-1;
+	
+	//allcote pcb number
+	private static int PidMaxIndex=-1;
+	public static int PageTableMaxIndex=-1;
 	public static Kernal createNewInstance(){
 		if(kernal==null)
 			kernal=new Kernal();
+		
+		//load conf
+		Properties properties=ConfLoader.getPropertiesByConfType(ConfType.Kernal);
+		LABN=Integer.valueOf(properties.getProperty("LABN"));
+		PABN=Integer.valueOf(properties.getProperty("PABN"));
+		switch (properties.getProperty("ReplacePolicy")) {
+		case "FIFO":
+			replacePolicy=FIFO;
+			break;
+		case "LRU":
+			replacePolicy=LRU;
+			break;
+		case "RANDOM":
+			replacePolicy=RANDOM;
+			break;
+		default:
+			System.err.println("error data from ConfLoader.Kernal");
+			break;
+		}
 		return kernal;
 	}
 	public static Kernal getInstance(){
 		return kernal;
 	}
 	public PCB allocatePCB(){
-		return null;
+		PidMaxIndex++;
+		PageTableMaxIndex++;
+		PCB pcb=new PCB(PidMaxIndex,PageTableMaxIndex);
+		return pcb;
 	}
 }

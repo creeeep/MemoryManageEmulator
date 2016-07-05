6bbc395f4b40894dad5fe00f1cba44240e5b14a3
arics <aricsfaraway@gmail.com>
2016-07-05 10:42:01 +0800

    Add process's access time and wait time;Add process test

---
+++ src/test/ProcessTest.java
@@ -0,0 +1,12 @@
+package test;
+import inter.Process;
+public class ProcessTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Process process=new Process();
+		process.accessNext();
+
+	}
+
+}
